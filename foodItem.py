8821d3badbd02f90e6beede8f49b75278ed61592
Paul Bekaert <bekaert.paul@gmail.com>
2021-04-08 19:15:48 +0100

    fixed stuff

--- foodItem.py
+++
@@ -1,9 +0,0 @@
-class FoodItem:
-
-	def __init__(self, name, buyQuantity, price):
-		self.name = name
-		self.quantity = buyQuantity
-		self.price = price
-
-	def __str__(self):
-		return "[" + self.name + "," + str(self.quantity) + "," + str(self.price) + "]"
