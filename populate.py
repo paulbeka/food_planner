d9c303a38b106388f954fce4bcba2290ad1b0b26
Paul Bekaert <2517073B@student.gla.ac.uk>
2021-04-19 19:13:07 +0100

    made a better population script

--- populate.py
+++ populate.py
@@ -1,38 +1,56 @@
-from food_planner.meal import Meal
-from food_planner.foodItem import FoodItem
-from food_planner.tools import updateMeal, updateCupboard
+from food_planner.settings import ITEM_LOC, PRICE_LOC, MEAL_LOC
 
+
+PRICES = """chicken2breast,300.0,0.006
+eggs,12.0,0.170833333
+ham,10.0,0.1
+tortilla_wraps,8.0,0.11875
+edam,10.0,0.175
+2avocado,2.0,1.25
+4_pork_loins,4.0,0.75
+petit_pois,1000.0,0.0013
+baby_potatoes,1000.0,0.001
+asparagus_tips,125.0,0.0144
+2_salmon,2.0,1.625
+spagetti,1000.0,0.001
+bolognese,1000.0,0.00064
+smoked_bacon_lardons,2.0,1.0
+sausages,12.0,0.20833333333333334
+carrots,1000.0,0.0004
+couscous,500.0,0.0014
+red_onions,3.0,0.2833333333333333
+peppers,3.0,0.45
+minced_beef,500.0,0.005
+corn_can,200.0,0.0034999999999999996
+mixed_frozen_veggies,1000.0,0.00079
+sliced_bread,20.0,0.05500000000000001
+hash_browns,15.0,0.08
+gnocci,500.0,0.0015
+pepperoni_pizza,1.0,3.5
+meal_deal,1.0,3.0
+"""
+
+MEALS = """tortillas,tortilla_wraps:2.0:0.11875,ham:5.0:0.1,edam:2.0:0.175,2avocado:0.5:1.25
+pork_hash_petitpois,4_pork_loins:2.0:0.75,petit_pois:200.0:0.0013,hash_browns:5.0:0.08
+spaghetti bolognese,spagetti:200.0:0.001,bolognese:250.0:0.00064,minced_beef:500.0:0.005
+beef and corn,corn_can:200.0:0.0034999999999999996,minced_beef:250.0:0.005,couscous:150.0:0.0014
+eggs and potatoes,eggs:3.0:0.170833333,baby_potatoes:330.0:0.001
+eggs_poatoes_carrots,baby_potatoes:330.0:0.001,eggs:3.0:0.170833333,carrots:200.0:0.0004
+carbonara,spagetti:200.0:0.001,smoked_bacon_lardons:1.0:1.0,red_onions:0.5:0.2833333333333333,eggs:1.0:0.170833333
+sasage_veggies_potatoes,sausages:6.0:0.20833333333333334,baby_potatoes:330.0:0.001,mixed_frozen_veggies:250.0:0.00079
+"""
+CUPBOARD = """"""
 
 def main():
 
-	foodList = []
-	mealList = []
-	priceList = []
+	with open(ITEM_LOC, "w") as f:
+		f.write(PRICES)
 
-	foodDict = {}
+	with open(PRICE_LOC, "w") as f:
+		f.write(PRICES)
 
-	foodList.append(FoodItem("chicken",300,2/300))
-	foodList.append(FoodItem("steak",250,5/250))
-	foodList.append(FoodItem("pork",300,2/300))
-	foodList.append(FoodItem("onions",100,1.6/100))
-	foodList.append(FoodItem("asparagus",50,2/50))
-	foodList.append(FoodItem("nutella",1000,4.5/1000))
-	foodList.append(FoodItem("pizza",250,2.5/250))
-	foodList.append(FoodItem("tortilla", 8, 8/2))
-
-	for item in foodList:
-		foodDict[item.name] = item
-
-	mealList.append(Meal("chicken_onions", [FoodItem("chicken", 500, 2/200), foodList[3]]))
-	mealList.append(Meal("pork_asparagus", [foodList[2], foodList[4]]))
-	mealList.append(Meal("pizza_chicken", [FoodItem("chicken", 500, 2/200), foodList[6]]))
-	mealList.append(Meal("nutella_steak", [FoodItem("nutella", 50, 4.5/1000), FoodItem("steak", 50, 5/250)]))
-	mealList.append(Meal("chicken_steak", [FoodItem("chicken", 500, 2/200), FoodItem("steak", 50, 5/250)]))
-	mealList.append(Meal("tortilla_tortilla", [FoodItem("tortilla",2,2/8)]))
-
-
-	updateCupboard(foodDict)
-	updateMeal(mealList)
+	with open(MEAL_LOC, "w") as f:
+		f.write(MEALS)
 
 
 if __name__ == "__main__":
